3750374417a4c1f18ef9be75afe603fe67f5e104
Mhiw <wil.carlsson@gmail.com>
2022-10-09 17:03:31 +0200

    Added start button

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
+
+    public Main() {
+        JFrame frame = new JFrame();
+        frame.setTitle("Timer");
+        frame.setSize(400, 200);
+        frame.setAlwaysOnTop(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton StartBtn = new JButton("Start");
+        StartBtn.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Start");
+                }
+            }
+        );
+
+        frame.setLayout(new FlowLayout());
+        frame.add(StartBtn);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
     }
 }
